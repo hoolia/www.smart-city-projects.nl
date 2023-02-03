ff3e73d3461215f10ea0fa028bf309a1d2987fd6
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2023-02-03 11:56:31 +0100

    .

---
+++ docs/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[...slug]","\u002Fhelp","\u002Fhelp\u002F[category]","\u002Fhelp\u002F[category]\u002F[slug]","\u002Fpricing","\u002Fproduct"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
